c3e9152f9018fd2121e26a8762e1a83cee96393d
Aundra Miller <miller.aundra@gmail.com>
2022-09-22 18:40:21 -0700

    Alternative solution with an iterator

--- blanket_pattern.rb
+++ blanket_pattern.rb
@@ -2,9 +2,18 @@ pattern = "~~**~~//..//~~**~~_-_-_"
 
 row_count = 100
 
-while row_count > 0
-    first_element = pattern.slice(0)
-    pattern.delete_prefix!(first_element)
-    puts pattern += pattern.slice(0)
-    row_count -= 1
+# while row_count > 0
+#     first_element = pattern.slice(0)
+#     pattern.delete_prefix!(first_element)
+#     puts pattern += pattern.slice(0)
+#     row_count -= 1
+# end
+
+# iterator solution instead for practice
+
+row_count.times do |i|
+    first_element = pattern[0]
+    remaining_elements = pattern[1..-1]
+    pattern = remaining_elements + first_element
+    puts pattern
 end
